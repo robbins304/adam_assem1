c17c0c74bcd62dffdacac09df4fe4c61bc728c02
robbins304 <131439520+robbins304@users.noreply.github.com>
2023-12-07 16:04:43 +0900

    Update display.launch.py (added missing ParameterValue argument to launch_ros.descriptions)

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': launch_ros.descriptions(Command(['xacro ', LaunchConfiguration('model')]))}]
+        parameters=[{'robot_description': launch_ros.descriptions.ParameterValue(Command(['xacro ', LaunchConfiguration('model')]), value_type=str)}]
     )
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
